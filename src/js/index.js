b9211ef4c2f811053cf9a5589725b6fbc74a72a1
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-08-23 10:27:42 -0600

    arreglos nuevos

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
-import {inicio} from'../../../../myFrameWoks/myStyleGuide/src/js/index.js'
-
+//import {inicio} from'../../../../myFrameWoks/myStyleGuide/src/js/index.js'
+import {Prism} from '../../vendors/prism/prism'
 
 (()=>{
 	inicio();
+	Prism();
 	if (document.body.classList.contains('home')) {
 		// functions here
 	}else if (document.body.classList.contains('page2')) {
