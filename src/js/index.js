04b86357c883942e7034dbc2fe0388873c5613a1
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-09-22 22:59:29 -0600

    ajustes de limpieza

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,4 @@
-//import {inicio} from'../../../../myFrameWoks/myStyleGuide/src/js/index.js'
-import {Prism} from '../../vendors/prism/prism'
-
 (()=>{
-	inicio();
-	Prism();
 	if (document.body.classList.contains('home')) {
 		// functions here
 	}else if (document.body.classList.contains('page2')) {
