e02dd0ec31b5c87f3363b6af0429dd42c822155c
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-11-26 14:15:49 -0600

    lightbox and scroll components included

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,19 @@
 import {topNav} from './modules/topNav'
-// import {marvelApi} from './modules/marvelApi'
+import {marvelApi} from './modules/marvelApi'
 import {searchFilter} from './modules/searchFilter'
+import {animatedScroll} from './modules/animatedScroll'
 
 (()=>{
 	topNav();
-	searchFilter();
+
 	if (document.body.classList.contains('home')) {
 		// functions here
 	}else if (document.body.classList.contains('page2')) {
 		// functions here
-		// marvelApi();
-
+		searchFilter();
+		marvelApi();
+	}else if (document.body.classList.contains('page3')) {
+		// functions here
+		animatedScroll();
 	}
 })();
