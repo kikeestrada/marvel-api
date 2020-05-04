b60a56adb101a0b4df1c63beb04e7b567fe8ebf3
kikeestrada <kikeestrada@gmail.com>
2020-05-04 03:33:08 -0600

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,2 @@
 import {marvelApi} from './modules/marvelApi';
-import {swDetecter} from './modules/swDetecter';
 	marvelApi();
-	swDetecter();
--- src/js/modules/swDetecter.js
+++
@@ -1,7 +0,0 @@
-export const swDetecter = () => {
-	if ('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('./sw.js')
-			.then(reg => console.log('Registro de SW exitoso', reg))
-			.catch(err => console.warn('Error al tratar de registrar el sw', err))
-	}
-};
