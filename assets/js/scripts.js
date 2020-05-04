f88e1f2dbe08da6a2aa8cbd17a1abfea23591bbc
kikeestrada <kikeestrada@gmail.com>
2020-05-04 01:11:12 -0600

    Updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -416,116 +416,23 @@ var _searchFilter = require('./modules/searchFilter');
 
 var _marvelApi = require('./modules/marvelApi');
 
-var _tabs = require('./modules/tabs');
-
-var _modal = require('./modules/modal');
+var _swDetecter = require('./modules/swDetecter');
 
 (function () {
 	(0, _topNav.topNav)();
+	(0, _swDetecter.swDetecter)();
 	if (document.body.classList.contains('home')) {
 		// functions here
 	} else if (document.body.classList.contains('page2')) {
+		// functions here
 		(0, _searchFilter.searchFilter)();
 		(0, _marvelApi.marvelApi)();
-		// functions here
 	} else if (document.body.classList.contains('page3')) {
-		// functions here
-	} else if (document.body.classList.contains('page4')) {
-		// functions here
-	} else if (document.body.classList.contains('page5')) {
-		// functions here
-	} else if (document.body.classList.contains('page6')) {
-		// functions here
-	} else if (document.body.classList.contains('page7')) {
-		// functions here
-		(0, _tabs.tabs)();
-		(0, _modal.edModal)();
-	} else if (document.body.classList.contains('page8')) {
 		// functions here
 	}
 })();
 
-},{"./modules/marvelApi":4,"./modules/modal":5,"./modules/searchFilter":6,"./modules/tabs":7,"./modules/topNav":8}],3:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-// Crear elementos con atributos e hijo
-var createCustomElement = exports.createCustomElement = function createCustomElement(element, attributes, children) {
-  var customElement = document.createElement(element);
-  if (children !== undefined) children.forEach(function (el) {
-    if (el.nodeType) {
-      if (el.nodeType === 1 || el.nodeType === 11) customElement.appendChild(el);
-    } else {
-      customElement.innerHTML += el;
-    }
-  });
-  addAttributes(customElement, attributes);
-  return customElement;
-};
-
-// Añadir un objeto de atributos a un elemento
-var addAttributes = exports.addAttributes = function addAttributes(element, attrObj) {
-  for (var attr in attrObj) {
-    if (attrObj.hasOwnProperty(attr)) element.setAttribute(attr, attrObj[attr]);
-  }
-};
-
-// Envolver un elemento con otro
-var wrap = exports.wrap = function wrap(selector, wrapElementType, attributesObj) {
-  var element = getElement(selector),
-      nextSibling = element.nextElementSibling,
-      parent = element.parentElement,
-      wrapElement = createCustomElement(wrapElementType, attributesObj, element);
-
-  nextSibling ? parent.insertBefore(wrapElement, nextSibling) : parent.appendChild(wrapElement);
-
-  return wrapElement;
-};
-
-// Retornar un elemento del DOM (revisar)
-var getElement = exports.getElement = function getElement(elementOrSelector) {
-  var e = void 0,
-      g = void 0;
-  if (elementOrSelector.nodeType === 1) {
-    e = elementOrSelector;
-  } else {
-    g = document.querySelector(elementOrSelector);
-    if (document.querySelector(g)) {
-      e = document.querySelector(g);
-    } else {
-      e = document.createElement('div');
-      console.error('Function getElement() requires a DOM element\n    or a valid selector. It has been created a placeholder element to avoid\n    execution errors, please fixed as soon as posible');
-    }
-  }
-  return e;
-};
-
-// Media queries
-var mediaQuery = function mediaQuery(breakpoint, cb) {
-  var isChangeSize = function isChangeSize(mql) {
-    return cb(mql.matches);
-  };
-  breakpoint.addListener(isChangeSize);
-  isChangeSize(breakpoint);
-};
-
-// From (EDgrid equivalent)
-// cb receive a boolean argument from mediaQuery() function
-var from = function from(breakpoint, cb) {
-  var bp = window.matchMedia('(min-width: ' + breakpoint + ')');
-  mediaQuery(bp, cb);
-};
-
-// To (EDgrid equivalent)
-// cb receive a boolean argument from mediaQuery() function
-var to = function to(breakpoint, cb) {
-  var bp = window.matchMedia('(max-width: ' + breakpoint + ')');
-  mediaQuery(bp, cb);
-};
-
-},{}],4:[function(require,module,exports){
+},{"./modules/marvelApi":3,"./modules/searchFilter":4,"./modules/swDetecter":5,"./modules/topNav":6}],3:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -597,52 +504,7 @@ var marvelApi = exports.marvelApi = function marvelApi() {
 	getConnection();
 };
 
-},{"../../../node_modules/blueimp-md5/js/md5":1}],5:[function(require,module,exports){
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-exports.edModal = undefined;
-
-var _helpers = require('./helpers');
-
-var edModal = exports.edModal = function edModal() {
-	// Imprimir modal
-	var printModal = function printModal(content) {
-		// crear contenedor interno
-		var modalContentEl = (0, _helpers.createCustomElement)('div', {
-			id: 'ed-modal-content',
-			class: 'ed-modal-content'
-		}, [content]),
-
-
-		// crear contenedor principal
-		modalContainerEl = (0, _helpers.createCustomElement)('div', {
-			id: 'ed-modal-container',
-			class: 'ed-modal-container'
-		}, [modalContentEl]);
-
-		// Imprimir el modal
-		document.body.appendChild(modalContainerEl);
-
-		// Remover el modal
-		var removeModal = function removeModal() {
-			return document.body.removeChild(modalContainerEl);
-		};
-
-		modalContainerEl.addEventListener('click', function (e) {
-			if (e.target === modalContainerEl) removeModal();
-		});
-	};
-
-	var saludo = '<h1>This is my Modal Component</h1>';
-	document.getElementById('show-modal').addEventListener('click', function () {
-		printModal(saludo);
-	});
-};
-
-},{"./helpers":3}],6:[function(require,module,exports){
+},{"../../../node_modules/blueimp-md5/js/md5":1}],4:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -669,60 +531,36 @@ var searchFilter = exports.searchFilter = function searchFilter() {
 	fnFilter(document.getElementById('searchInput'), '.class-item__fragment', '.class-item');
 };
 
-},{}],7:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
-
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
-var tabs = exports.tabs = function tabs() {
-	var container = document.querySelector('.edui-tabs'),
-	    tabsContainer = container.querySelector('.tabs'),
-	    panelsContainer = container.querySelector('.panels'),
-	    tabs = [].concat(_toConsumableArray(tabsContainer.querySelectorAll('.tab'))),
-	    panels = [].concat(_toConsumableArray(panelsContainer.querySelectorAll('.panel')));
-
-	tabs[0].classList.add('active');
-	panels[0].classList.add('active');
-
-	tabsContainer.addEventListener('click', function (e) {
-		var t = e.target,
-		    i = tabs.indexOf(t);
-		if (e.target.classList.contains('tab')) {
-			tabs.map(function (tab) {
-				return tab.classList.remove('active');
-			});
-			panels.map(function (panel) {
-				return panel.classList.remove('active');
-			});
-			t.classList.add('active');
-			panels[i].classList.add('active');
-		}
-	});
+var swDetecter = exports.swDetecter = function swDetecter() {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('./sw.js').then(function (reg) {
+			return console.log('Registro de SW exitoso', reg);
+		}).catch(function (err) {
+			return console.warn('Error al tratar de registrar el sw', err);
+		});
+	}
 };
 
-},{}],8:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 var topNav = exports.topNav = function topNav() {
-	var myFunction = function myFunction() {
-		document.querySelector('.hamburger').addEventListener('click', function (e) {
-			e.preventDefault();
-			[].map.call(document.querySelectorAll('.hamburger'), function (el) {
-				el.classList.toggle('is-active');
-			});
-			[].map.call(document.querySelectorAll('.top-nav__menu'), function (el) {
-				el.classList.toggle('show-top-nav');
-			});
-		});
-	};
-	myFunction();
+	var d = document,
+	    headerBtn = d.querySelector('.hamburger'),
+	    nav = d.querySelector('.top-nav__menu');
+	headerBtn.addEventListener('click', function (e) {
+		e.preventDefault();
+		headerBtn.classList.toggle('is-active'), nav.classList.toggle('show-top-nav');
+	});
 };
 
 },{}]},{},[2]);
